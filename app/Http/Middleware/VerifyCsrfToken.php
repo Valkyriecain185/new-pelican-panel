a1e2d6ac8d36e67b093c147a32446fa8e93e85fb
Sentinel Development <148332910+Valkyriecain185@users.noreply.github.com>
2026-03-29 03:40:36 +0100

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,11 +6,9 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
-    /**
-     * The URIs that should be excluded from CSRF verification. These are
-     * never hit by the front-end, and require specific token validation
-     * to work.
-     */
-    protected $except = ['remote/*', 'daemon/*'];
-    protected $except = ['stripe/webhook',];
+    protected $except = [
+        'remote/*',
+        'daemon/*',
+        'stripe/webhook',
+    ];
 }
