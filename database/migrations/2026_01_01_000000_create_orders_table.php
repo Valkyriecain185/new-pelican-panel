a58e3c9f01dd40734f293d89d5108eda00fbaf6a
Sentinel Development <148332910+Valkyriecain185@users.noreply.github.com>
2026-03-29 03:16:20 +0100

    Update 2026_01_01_000000_create_orders_table.php
    fixes

--- database/migrations/2026_01_01_000000_create_orders_table.php
+++ database/migrations/2026_01_01_000000_create_orders_table.php
@@ -8,8 +8,7 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('plan');
             $table->string('billing');
             $table->integer('amount');
